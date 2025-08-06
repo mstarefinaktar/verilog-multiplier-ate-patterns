0e8f7d726a6970c09ee0a9c187ff9505bf1d44e6
Mst Arefin Aktar <100572377+mstarefinaktar@users.noreply.github.com>
2025-08-06 13:11:17 +0600

    id_stage.v

--- legacy_mips/id_stage.v
+++ legacy_mips/id_stage.v
@@ -1,1 +1,8 @@
-// Placeholder for id_stage.v
+module id_stage (
+    input [31:0] instruction,
+    output [4:0] rs, rt, rd
+);
+    assign rs = instruction[25:21];
+    assign rt = instruction[20:16];
+    assign rd = instruction[15:11];
+endmodule
