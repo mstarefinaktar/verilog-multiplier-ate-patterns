451044d95410629cff020ae77f9e4e0570448c67
Mst Arefin Aktar <100572377+mstarefinaktar@users.noreply.github.com>
2025-08-06 13:10:00 +0600

    testbench.v

--- legacy_mips/testbench.v
+++ legacy_mips/testbench.v
@@ -1,1 +1,22 @@
 
+`timescale 1ns / 1ps
+
+module testbench;
+    reg clk = 0;
+    reg reset = 1;
+
+    always #5 clk = ~clk;
+
+    mips_top DUT (
+        .clk(clk),
+        .reset(reset)
+    );
+
+    initial begin
+        $dumpfile("wave.vcd");
+        $dumpvars(0, testbench);
+
+        #10 reset = 0;
+        #100 $finish;
+    end
+endmodule
